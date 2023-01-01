ed73d89fad98b18b1fe54bbcfbe985cdd47a440b
sayf <sayf.jawad@alliander.com>
2023-01-02 00:43:26 +0100

    Making the code even more horrible with duplicate code

--- src/main/java/nl/multicode/Application.java
+++ src/main/java/nl/multicode/Application.java
@@ -11,7 +11,29 @@ public class Application {
 
         if (args[0] != null && args[1] != null) {
             if ("validate".equals(args[0]) && args[2] != null && (args[2].length() == 9 || args[2].length() == 10)) {
-                if (validate(args)) {
+                boolean result = false;
+                boolean finished = false;
+
+                int[] m = new int[0];
+                int j = 0;
+                if ("bsn".equals(args[1]) && args[2].length() == 9) {
+                    j = 9;
+                    m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
+                } else if ("bank".equals(args[1]) && args[2].length() == 10) {
+                    j = 10;
+                    m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+                } else {
+                    finished = true;
+                }
+                if (!finished) {
+                    int sum = 0;
+                    for (int i = 0; i < j; i++) {
+                        sum += Character.getNumericValue(args[2].toCharArray()[i]) * m[i];
+                    }
+                    result = (sum % 11 == 0);
+                }
+
+                if (result) {
                     log.info(args[2] + " is a valid " + args[1]);
                 } else {
                     log.info(args[2] + " is an invalid " + args[1]);
@@ -26,7 +48,30 @@ public class Application {
                             n += (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
                         }
                         String randomNumber = n;
-                        if (validate(new String[]{"validate", "bsn", randomNumber})) {
+                        boolean result = false;
+                        boolean finished = false;
+                        String[] args1 = new String[]{"validate", "bsn", randomNumber};
+
+                        int[] m = new int[0];
+                        int j = 0;
+                        if ("bsn".equals(args1[1]) && args1[2].length() == 9) {
+                            j = 9;
+                            m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
+                        } else if ("bank".equals(args1[1]) && args1[2].length() == 10) {
+                            j = 10;
+                            m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+                        } else {
+                            finished = true;
+                        }
+                        if (!finished) {
+                            int sum = 0;
+                            for (int i = 0; i < j; i++) {
+                                sum += Character.getNumericValue(args1[2].toCharArray()[i]) * m[i];
+                            }
+                            result = (sum % 11 == 0);
+                        }
+
+                        if (result) {
                             log.info("generated :" + randomNumber);
                             break;
                         }
@@ -40,7 +85,30 @@ public class Application {
                             n += (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
                         }
                         String randomNumber = n;
-                        if (validate(new String[]{"validate", "bank", randomNumber})) {
+                        boolean result = false;
+                        boolean finished = false;
+                        String[] args1 = new String[]{"validate", "bank", randomNumber};
+
+                        int[] m = new int[0];
+                        int j = 0;
+                        if ("bsn".equals(args1[1]) && args1[2].length() == 9) {
+                            j = 9;
+                            m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
+                        } else if ("bank".equals(args1[1]) && args1[2].length() == 10) {
+                            j = 10;
+                            m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+                        } else {
+                            finished = true;
+                        }
+                        if (!finished) {
+                            int sum = 0;
+                            for (int i = 0; i < j; i++) {
+                                sum += Character.getNumericValue(args1[2].toCharArray()[i]) * m[i];
+                            }
+                            result = (sum % 11 == 0);
+                        }
+
+                        if (result) {
                             log.info("generated :" + randomNumber);
                             break;
                         }
@@ -50,24 +118,4 @@ public class Application {
         }
     }
 
-    private static boolean validate(String[] args) {
-
-        int[] m;
-        int j;
-        if ("bsn".equals(args[1]) && args[2].length() == 9) {
-            j = 9;
-            m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
-        } else if ("bank".equals(args[1]) && args[2].length() == 10) {
-            j = 10;
-            m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        } else {
-            return false;
-        }
-
-        int sum = 0;
-        for (int i = 0; i < j; i++) {
-            sum += Character.getNumericValue(args[2].toCharArray()[i]) * m[i];
-        }
-        return (sum % 11 == 0);
-    }
 }
