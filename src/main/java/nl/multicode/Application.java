3bc781cf5a530afe264ec340a85b06f01e8a8e46
sayf <sayf.jawad@alliander.com>
2023-01-10 17:23:12 +0100

    Simple complexity displacement example

--- src/main/java/nl/multicode/Application.java
+++ src/main/java/nl/multicode/Application.java
@@ -9,25 +9,22 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println(new Application().execute("combine", "dirty", "Code"));
-        System.out.println(new Application().execute("combineToUppercase", "dirty", "Code"));
-        System.out.println(new Application().execute("combineToUppercase", null, "Code"));
-
+        new Application().execute("combine", "dirty", "Code");
+        new Application().execute("combineToUppercase", "dirty", "Code");
+        new Application().execute("combineToUppercase", null, "Code");
     }
 
     public String execute(String command, String firstArgument, String secondArgument) {
 
         if (command != null && command.equals("combine")) {
             if (firstArgument != null && secondArgument != null) {
-                final var message = String.format("Combined firstArgument '%s' and secondArgument '%s' and resulted in '%s'", firstArgument, secondArgument, firstArgument + secondArgument);
-                System.out.println(message);
+                log.info("Combined firstArgument '{}' and secondArgument '{}' and resulted in '{}'", firstArgument, secondArgument, firstArgument + secondArgument);
                 return firstArgument + secondArgument;
             }
         } else if (command != null && command.equals("combineToUppercase")) {
             if (firstArgument != null && secondArgument != null) {
                 final var combinedToUppercase = firstArgument.toUpperCase() + "_" + secondArgument.toUpperCase();
-                final var message = String.format("Combined to uppercase firstArgument '%s' and secondArgument '%s' resulted in '%s'", firstArgument, secondArgument, combinedToUppercase);
-                System.out.println(message);
+                log.info("Combined to uppercase firstArgument '{}' and secondArgument '{}' resulted in '{}'", firstArgument, secondArgument, combinedToUppercase);
                 return combinedToUppercase;
             }
         }
