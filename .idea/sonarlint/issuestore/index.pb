38d123921be33ab8e1df3e6b7b3afd4c6f3d47e7
sayf <sayf.jawad@alliander.com>
2023-01-10 17:18:38 +0100

    Simple complexity displacement example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,12 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+_
+/src/test/java/nl/multicode/ApplicationTest.java,5/a/5a8cdee52be9ff77b03c1916d349289ed13b9c6c
+[
++src/main/java/nl/multicode/Application.java,0/7/07d47e2f423c92f2f81ee4c88110d6a6a1fd07d0
--- src/main/java/nl/multicode/Application.java
+++ src/main/java/nl/multicode/Application.java
@@ -9,112 +9,28 @@ public class Application {
 
     public static void main(String[] args) {
 
-        if (args[0] != null && args[1] != null) {
-            if ("validate".equals(args[0]) && args[2] != null && (args[2].length() == 9 || args[2].length() == 10)) {
-                boolean result = false;
-                boolean finished = false;
+        System.out.println(new Application().execute("combine", "dirty", "Code"));
+        System.out.println(new Application().execute("combineToUppercase", "dirty", "Code"));
+        System.out.println(new Application().execute("combineToUppercase", null, "Code"));
 
-                int[] m = new int[0];
-                int j = 0;
-                if ("bsn".equals(args[1]) && args[2].length() == 9) {
-                    j = 9;
-                    m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
-                } else if ("bank".equals(args[1]) && args[2].length() == 10) {
-                    j = 10;
-                    m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-                } else {
-                    finished = true;
-                }
-                if (!finished) {
-                    int sum = 0;
-                    for (int i = 0; i < j; i++) {
-                        sum += Character.getNumericValue(args[2].toCharArray()[i]) * m[i];
-                    }
-                    result = (sum % 11 == 0);
-                }
+    }
 
-                if (result) {
-                    log.info("{} is a valid {}", args[2], args[1]);
-                } else {
-                    log.info("{} is an invalid {}", args[2], args[1]);
-                }
-            } else if ("generate".equals(args[0])) {
-                if ("bsn".equals(args[1])) {
-                    int l = 9;
-                    while (true) {
+    public String execute(String command, String firstArgument, String secondArgument) {
 
-                        String n = "";
-                        for (int i = 0; i < l; i++) {
-                            n += (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
-                        }
-                        String randomNumber = n;
-                        boolean result = false;
-                        boolean finished = false;
-                        String[] args1 = new String[]{"validate", "bsn", randomNumber};
-
-                        int[] m = new int[0];
-                        int j = 0;
-                        if ("bsn".equals(args1[1]) && args1[2].length() == 9) {
-                            j = 9;
-                            m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
-                        } else if ("bank".equals(args1[1]) && args1[2].length() == 10) {
-                            j = 10;
-                            m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-                        } else {
-                            finished = true;
-                        }
-                        if (!finished) {
-                            int sum = 0;
-                            for (int i = 0; i < j; i++) {
-                                sum += Character.getNumericValue(args1[2].toCharArray()[i]) * m[i];
-                            }
-                            result = (sum % 11 == 0);
-                        }
-
-                        if (result) {
-                            log.info("generated :{}", randomNumber);
-                            break;
-                        }
-                    }
-                } else if ("bank".equals(args[1])) {
-                    int l = 10;
-                    while (true) {
-
-                        String n = "";
-                        for (int i = 0; i < l; i++) {
-                            n += (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
-                        }
-                        String randomNumber = n;
-                        boolean result = false;
-                        boolean finished = false;
-                        String[] args1 = new String[]{"validate", "bank", randomNumber};
-
-                        int[] m = new int[0];
-                        int j = 0;
-                        if ("bsn".equals(args1[1]) && args1[2].length() == 9) {
-                            j = 9;
-                            m = new int[]{9, 8, 7, 6, 5, 4, 3, 2, -1};
-                        } else if ("bank".equals(args1[1]) && args1[2].length() == 10) {
-                            j = 10;
-                            m = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-                        } else {
-                            finished = true;
-                        }
-                        if (!finished) {
-                            int sum = 0;
-                            for (int i = 0; i < j; i++) {
-                                sum += Character.getNumericValue(args1[2].toCharArray()[i]) * m[i];
-                            }
-                            result = (sum % 11 == 0);
-                        }
-
-                        if (result) {
-                            log.info("generated :{}", randomNumber);
-                            break;
-                        }
-                    }
-                }
+        if (command != null && command.equals("combine")) {
+            if (firstArgument != null && secondArgument != null) {
+                final var message = String.format("Combined firstArgument '%s' and secondArgument '%s' and resulted in '%s'", firstArgument, secondArgument, firstArgument + secondArgument);
+                System.out.println(message);
+                return firstArgument + secondArgument;
+            }
+        } else if (command != null && command.equals("combineToUppercase")) {
+            if (firstArgument != null && secondArgument != null) {
+                final var combinedToUppercase = firstArgument.toUpperCase() + "_" + secondArgument.toUpperCase();
+                final var message = String.format("Combined to uppercase firstArgument '%s' and secondArgument '%s' resulted in '%s'", firstArgument, secondArgument, combinedToUppercase);
+                System.out.println(message);
+                return combinedToUppercase;
             }
         }
+        return null;
     }
 }
