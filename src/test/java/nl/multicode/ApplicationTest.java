780b68743eef52df1968622ace4a28cdf00881a2
sayf <sayf.jawad@alliander.com>
2023-01-19 11:30:10 +0100

    More test coverage

--- src/test/java/nl/multicode/ApplicationTest.java
+++ src/test/java/nl/multicode/ApplicationTest.java
@@ -1,5 +1,6 @@
 package nl.multicode;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 import nl.multicode.util.TestAppender;
 import org.apache.logging.log4j.Level;
@@ -7,14 +8,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ApplicationTest {
 
     private Application application;
 
     @BeforeEach
     public void setup() {
+
         application = new Application();
         TestAppender.clear();
     }
@@ -26,48 +26,65 @@ class ApplicationTest {
     }
 
     @Test
+    void null_input() {
+
+        application.getAnimalSound(null);
+        assertThat(application.getAnimalSound(null)).isEqualTo("null animal argument is not allowed!");
+        assertThat(TestAppender.count()).isZero();
+    }
+
+    @Test
     void logging() {
+
         application.getAnimalSound("unknown");
         assertThat(TestAppender.getLogs(Level.INFO).get(0)).isEqualTo("requesting sound of unknown");
     }
 
     @Test
     void cat() {
+
         assertThat(application.getAnimalSound("cat")).isEqualTo("meow");
     }
 
     @Test
     void dog() {
+
         assertThat(application.getAnimalSound("dog")).isEqualTo("bark");
     }
 
     @Test
     void dolphin() {
+
         assertThat(application.getAnimalSound("dolphin")).isEqualTo("click");
     }
 
     @Test
     void cow() {
+
         assertThat(application.getAnimalSound("cow")).isEqualTo("moo");
     }
 
     @Test
     void hyena() {
+
         assertThat(application.getAnimalSound("hyena")).isEqualTo("laugh");
     }
 
     @Test
     void dove() {
+
         assertThat(application.getAnimalSound("dove")).isEqualTo("coo");
     }
 
     @Test
     void chicken() {
+
         assertThat(application.getAnimalSound("chicken")).isEqualTo("cackle");
     }
 
     @Test
     void unknown() {
+
         assertThat(application.getAnimalSound("something")).isEqualTo("unknown");
     }
 }
