81c0b7b60eeb1c692debe8940d4351b43d0e0fcb
sayf <sayf.jawad@alliander.com>
2023-02-07 12:43:51 +0100

    Preparing the functional programming example

--- src/test/java/nl/multicode/ApplicationTest.java
+++ src/test/java/nl/multicode/ApplicationTest.java
@@ -28,35 +28,13 @@ class ApplicationTest {
 
   @Test
   void main_valid_bsn() {
-    Application.main(new String[]{"validate", "bsn", "218996755"});
+    Application.main(new String[]{ "218996755"});
     assertThat(TestAppender.getLogs(Level.INFO).get(0)).contains("218996755 is a valid bsn");
   }
 
   @Test
-  void main_valid_bank() {
-    Application.main(new String[]{"validate", "bank", "1015008577"});
-    assertThat(TestAppender.getLogs(Level.INFO).get(0)).contains("1015008577 is a valid bank");
-  }
-
-  @Test
   void main_invalid_bsn() {
-    Application.main(new String[]{"validate", "bsn", "228996755"});
+    Application.main(new String[]{"228996755"});
     assertThat(TestAppender.getLogs(Level.INFO).get(0)).contains("228996755 is an invalid bsn");
-  }
-
-  @Test
-  void main_invalid_bank() {
-    Application.main(new String[]{"validate", "bank", "1115008577"});
-    assertThat(TestAppender.getLogs(Level.INFO).get(0)).contains("1115008577 is an invalid bank");
-  }
-
-  @Test
-  void main_generate_bsn() {
-    Application.main(new String[]{"generate", "bsn"});
-    assertThat(TestAppender.getLogs(Level.INFO).get(0).matches("^\\bgenerated :\\d{9}$")).isTrue();
-  }  @Test
-  void main_generate_bank() {
-    Application.main(new String[]{"generate", "bank"});
-    assertThat(TestAppender.getLogs(Level.INFO).get(0).matches("^\\bgenerated :\\d{10}$")).isTrue();
   }
 }
