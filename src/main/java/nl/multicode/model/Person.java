b0b0fc48e579f1c3197394173382fe8e9efdcb64
sayfjawad <sayf@multicode.nl>
2023-03-12 22:10:43 +0100

    clean up unused imports

--- src/main/java/nl/multicode/model/Person.java
+++ src/main/java/nl/multicode/model/Person.java
@@ -2,7 +2,6 @@ package nl.multicode.model;
 
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/nl/multicode/service/CsvService.java
+++ src/main/java/nl/multicode/service/CsvService.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import nl.multicode.model.Person;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
